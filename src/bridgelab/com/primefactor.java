92e5671c005cd3a3bef026abde05f2c44826f2e5
rahul yadav <ry219613@gmail.com>
2021-10-08 03:22:50 +0530

    print prime factor number

---
+++ src/bridgelab/com/primefactor.java
@@ -0,0 +1,33 @@
+package bridgelab.com;
+
+import java.util.Scanner;
+
+public class primefactor {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int givenNumber;
+		int temp;
+		int smallestPrimeNumber = 2;
+		System.out.print("Enter any number: ");
+		givenNumber = scanner.nextInt();
+		temp = givenNumber;
+		
+		System.out.print("Prime Factor of Number : ");
+		while(temp > 1) {
+			if(temp % smallestPrimeNumber == 0) {
+				System.out.print(smallestPrimeNumber + " ,");
+				temp = temp / smallestPrimeNumber;
+			}
+			else {
+				smallestPrimeNumber++;
+			}
+		}
+		
+	}
+	
+
+
+	
+}
+
+
